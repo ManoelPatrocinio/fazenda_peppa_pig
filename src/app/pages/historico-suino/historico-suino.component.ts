df2f92ee53cbfb033a601d2d968ef8b6c626e9d4
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2024-04-22 18:40:40 -0300

    correção

--- src/app/pages/historico-suino/historico-suino.component.ts
+++ src/app/pages/historico-suino/historico-suino.component.ts
@@ -15,7 +15,7 @@ interface IHistorico {
 @Component({
   selector: 'app-historico-suino',
   standalone: true,
-  imports: [NgFor, NgIf, FormatdatePipe, LineChartComponent,BarChartComponent],
+  imports: [NgFor, NgIf, FormatdatePipe, LineChartComponent, BarChartComponent],
   templateUrl: './historico-suino.component.html',
   styleUrl: './historico-suino.component.css'
 })
@@ -29,7 +29,7 @@ export class HistoricoSuinoComponent {
   noData: boolean = false;
   pesos: number[] = [];
   datas: string[] = []
-  historicoAtividades:any[] = []
+  historicoAtividades: any[] = []
 
   constructor(private apiService: ApiService) { }
 
@@ -41,49 +41,18 @@ export class HistoricoSuinoComponent {
     let SuinoId = event.target.value
 
     if (SuinoId) {
-      this.historico = []
-      this.suinoSessaoHistorico = []
-      this.suinopesoHistorico = []
-      this.historicoAtividades = []
+      this.resertVariables()
     }
 
-    // retorna  a lista de pesos do suíno selecionado
     this.apiService.getSuinoById(SuinoId).subscribe((data) => {
       if (data != null) {
-        console.log("suino data: ", data)
         this.apiService.getListPesoSuinos().subscribe((response) => {
           this.suinopesoHistorico = response.filter((suino) => suino.suinoBrinco == data.Brinco.toString())
         })
-        // retorna  a lista das sessões do suino selecionado
         this.apiService.getListSessoes().subscribe((response: type_sessao[]) => {
+          this.AdiconarAtividadeHistorico(data, response);
+          this.AdicionarPesoHistorico()
 
-          const novoArray = response.filter(item => {
-            return item.s_animais.some(animal => animal.suinoBrinco === data.Brinco.toString());
-          }).map(item => {
-            return {
-              atividades: item.s_animais.filter(animal => animal.suinoBrinco === data.Brinco.toString())[0].atividades,
-              s_date: item.s_date
-            };
-          });
-
-          novoArray.forEach(item => {
-            item.atividades.forEach(atividade => {
-              const detalhe = atividade.atv_status ? "Concluido" : "Pendente";
-              this.historico.push({
-                data: new Date(item.s_date),
-                atividade: atividade.atv_name,
-                detalhe: detalhe
-              });
-            });
-          });
-          // adicionando dados da pesagem ao array de historico
-          this.suinopesoHistorico.forEach(pesagem => {
-            this.historico.push({
-              data: new Date(pesagem.DataPesagem),
-              atividade: "Pesagem",
-              detalhe: `${pesagem.suinoPeso} Kg`
-            });
-          });
           // ordenando em ordem  decrescente por data
           this.historico.sort((a, b) => b.data.getTime() - a.data.getTime());
           console.log("this.historico", this.historico)
@@ -118,7 +87,6 @@ export class HistoricoSuinoComponent {
 
   formatDatasToChartBar() {
 
-    if (this.historico.length > 0) {
       // Cria um objeto para armazenar a contagem de cada atividade
       let contadorAtividades: any = {};
 
@@ -133,6 +101,45 @@ export class HistoricoSuinoComponent {
         atividade,
         quantidade
       }));
-    }
+    
+  }
+
+  AdiconarAtividadeHistorico(data: type_suino, response: type_sessao[]) {
+    const novoArray = response.filter(item => {
+      return item.s_animais.some(animal => animal.suinoBrinco === data.Brinco.toString());
+    }).map(item => {
+      return {
+        atividades: item.s_animais.filter(animal => animal.suinoBrinco === data.Brinco.toString())[0].atividades,
+        s_date: item.s_date
+      };
+    });
+
+    novoArray.forEach(item => {
+      item.atividades.forEach(atividade => {
+        const detalhe = atividade.atv_status ? "Concluído" : "Pendente";
+        this.historico.push({
+          data: new Date(item.s_date),
+          atividade: atividade.atv_name,
+          detalhe: detalhe
+        });
+      });
+    });
+  }
+
+  AdicionarPesoHistorico() {
+    this.suinopesoHistorico.forEach(pesagem => {
+      this.historico.push({
+        data: new Date(pesagem.DataPesagem),
+        atividade: "Pesagem",
+        detalhe: `${pesagem.suinoPeso} Kg`
+      });
+    });
+  }
+
+  resertVariables(){
+    this.historico = []
+      this.suinoSessaoHistorico = []
+      this.suinopesoHistorico = []
+      this.historicoAtividades = []
   }
 }
